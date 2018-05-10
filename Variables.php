1fee48e3bc962b1999063fbe3eb48486fa51cacf
QA <qa@QAs-iMac.local>
2018-05-10 15:55:12 +0300

    Basic syntax and variables

---
+++ Variables.php
@@ -0,0 +1,21 @@
+<?php
+
+/**in all programing languages the variables are containers for storing data
+in PHP the variables are starting with $sign followed by the name of interface
+Every variable should start with a letter or underscore character and cannot
+start with a number.The variable names can contain only alpha-numeric characters
+and underscores
+Variable names are case sensitive
+
+*/
+$name = "This is string variable";
+echo $name
+
+$first = 4;
+$second = 5;
+
+echo $first + $second;
+
+
+
+ ?>
