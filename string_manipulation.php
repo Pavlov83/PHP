b0172d2cab18aa74f58a11a044f2510760f68ba2
QA <qa@QAs-iMac.local>
2018-05-11 11:41:44 +0300

    added string manipulation

---
+++ string_manipulation.php
@@ -0,0 +1,21 @@
+<?php
+echo strlen("Hello World!"); // outputs the amount of characters in a string
+?>
+
+<?php
+echo str_word_count("Wellcome to the world of PHP"); //outputs six
+ ?>
+
+<?php
+echo strrev("Hello World"); // outputs !dlrow olleH
+ ?>
+
+<?php
+echo strpos("Hello world","world"); /* outputs 6(the position where the
+searched word starts) */
+  ?>
+
+<?php
+
+echo str_replace("world","Luke", "Hello world");//outputs Hello Luke
+?>  
