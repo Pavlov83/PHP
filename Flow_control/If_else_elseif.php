d55274bd9a2010dc03dc10aaf68e3ed5cb13bd99
QA <qa@QAs-iMac.local>
2018-05-18 14:36:40 +0300

    examples

---
+++ Flow_control/If_else_elseif.php
@@ -0,0 +1,32 @@
+<?php
+
+$numOfOranges = 4;
+$numOfApples = 36;
+
+if(($numOfORanges > 25)&&($numOfApples>30)){
+
+    echo '25% Discount';
+
+} elseif (($numOfOranges > 30) ||($numOfApples >35)){
+
+    echo '15% Discount';
+
+} elseif (!(($numOfOrnages < 5)) || (!($numOfApples))) {
+
+    echo '5% Discount';
+
+} else {
+
+   echo 'No Discount for You';
+
+}
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ Flow_control/switch_statements.php
@@ -0,0 +1,32 @@
+<?php
+
+switch($usersName){
+
+ case "Pavel":
+    echo "Hello Pavel";
+    break;
+
+ case "Sally" :
+    echo "Hello Sally";
+    break;
+
+ default:
+    echo "Hello Valued Customer";
+    break;
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+ ?>
