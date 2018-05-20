ab72bc77cdffe5bebffe965fb0ad0dbbfcda31ac
Pavlov83 <codedriverun@gmail.com>
2018-05-20 09:26:37 +0300

    comparison operators added

---
+++ Course_excercises/Comparison_operators/Comparison_operators.php
@@ -0,0 +1,27 @@
+<?php
+
+==   //means equal to
+!=   //means not equal to
+>    //means greater than
+<    // means less than
+>=   //greater or equals
+<=   // less or equals
+
+if($name=='Batman'){
+
+  echo'The name is Batman';
+}
+
+$name = 'Batman';
+$age = 21;
+
+if($age>=21){
+  echo'TRUE.';
+}else{
+  echo'NOT True';
+}
+
+
+
+
+ ?>
